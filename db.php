7e1838e1d630d657a4f0f622cdefdc19b6fe6979
juan.martinez8799@ <juan.martinez8799@gmail.com>
2018-07-10 22:13:26 -0400

    restoring files from usb1

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 /*File to connect to the database*/
 
-$mysqli = new mysqli('localhost', 'root', 'Michael#77', 'nonnastable');
+$mysqli = new mysqli('localhost', 'root', '', 'nonnastable');
 /* check connection */
 if ($mysqli->connect_error) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
