ac0c0ca6925797d504edb125b55070de760d58b3
juan.martinez8799@ <juan.martinez8799@gmail.com>
2018-07-28 17:56:09 -0400

    Modified about page and replaced slider

--- js/functions.js
+++ js/functions.js
@@ -197,7 +197,7 @@ $('#return-to-top').click(function() {
     }, 500);
 });
 
-/*** REVIEWS SLIDER ***/
+/*** REVIEWS SLIDER **
 $("#slideshow > p:gt(0)").hide();
 
 setInterval(function() { 
@@ -208,6 +208,7 @@ setInterval(function() {
     .end()
     .appendTo('#slideshow');
 },  10000);
+*/
 
 /*** REDIRECT LOGO ***/
 document.getElementById("home").onclick = function () {
@@ -229,3 +230,95 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+/*Slide Show*/
+
+$('.slider').each(function() {
+  var $this = $(this);
+  var $group = $this.find('.slide_group');
+  var $slides = $this.find('.slide');
+  var bulletArray = [];
+  var currentIndex = 0;
+  var timeout;
+  
+  function move(newIndex) {
+    var animateLeft, slideLeft;
+    
+    advance();
+    
+    if ($group.is(':animated') || currentIndex === newIndex) {
+      return;
+    }
+    
+    bulletArray[currentIndex].removeClass('active');
+    bulletArray[newIndex].addClass('active');
+    
+    if (newIndex > currentIndex) {
+      slideLeft = '100%';
+      animateLeft = '-100%';
+    } else {
+      slideLeft = '-100%';
+      animateLeft = '100%';
+    }
+    
+    $slides.eq(newIndex).css({
+      display: 'block',
+      left: slideLeft
+    });
+    $group.animate({
+      left: animateLeft
+    }, function() {
+      $slides.eq(currentIndex).css({
+        display: 'none'
+      });
+      $slides.eq(newIndex).css({
+        left: 0
+      });
+      $group.css({
+        left: 0
+      });
+      currentIndex = newIndex;
+    });
+  }
+  
+  function advance() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      if (currentIndex < ($slides.length - 1)) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
+    }, 10000); // Modify advancement to next slide time
+  }
+  
+  $('.next_btn').on('click', function() {
+    if (currentIndex < ($slides.length - 1)) {
+      move(currentIndex + 1);
+    } else {
+      move(0);
+    }
+  });
+  
+  $('.previous_btn').on('click', function() {
+    if (currentIndex !== 0) {
+      move(currentIndex - 1);
+    } else {
+      move(3);
+    }
+  });
+  
+  $.each($slides, function(index) {
+    var $button = $('<a class="slide_btn">&bull;</a>');
+    
+    if (index === currentIndex) {
+      $button.addClass('active');
+    }
+    $button.on('click', function() {
+      move(index);
+    }).appendTo('.slide_buttons');
+    bulletArray.push($button);
+  });
+  
+  advance();
+});
